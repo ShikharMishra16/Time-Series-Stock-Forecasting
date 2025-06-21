0238c57c6a3c22317484a3c8244257cc7cad9b46
Shikhar Mishra <144837855+ShikharMishra16@users.noreply.github.com>
2025-06-21 14:21:46 +0530

    Update AAPLCleaning.py

--- Scripts/DataCleaning/AAPLCleaning.py
+++ Scripts/DataCleaning/AAPLCleaning.py
@@ -9,19 +9,5 @@ print("Shape:", df.shape)
 print(df.describe())
 print("Missing values:\n", df.isnull().sum())
 
-#Close Price
-plt.figure(figsize=(12,6))
-df['Close'].plot(title='AAPL Closing Price')
-plt.xlabel('Date')
-plt.ylabel('Price')
-plt.grid()
-plt.show()
-
-#Rolling thirty
-df['Rolling thirty'] = df['Close'].rolling(window=30).mean()
-plt.figure(figsize=(12,6))
-df[['Close', 'Rolling thirty']].plot(title='30-Day Average')
-plt.grid()
-plt.show()
-
+#Saving
 df.to_csv('AAPL_cleaned.csv')
