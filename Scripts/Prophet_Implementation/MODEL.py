6fb21bb4f0f861ac84495084dc0eb6ef18c44aca
Shikhar Mishra <144837855+ShikharMishra16@users.noreply.github.com>
2025-06-21 14:50:40 +0530

    Update and rename MODEL.py to MODEL.py

--- Scripts/Prophet_Implementation/MODEL.py
+++ Scripts/Prophet_Implementation/MODEL.py
@@ -6,22 +6,18 @@ df = pd.read_csv("AAPL_cleaned.csv", parse_dates=["Date"], dayfirst=True)
 #renaming columns for prophete
 df_prophet = df[["Date", "Close"]].rename(columns={"Date": "ds", "Close": "y"})
 
-# STEP 2: Initialize and Fit Prophet Model
+#Fitting the model
 model = Prophet()
 model.fit(df_prophet)
-
-# STEP 3: Make Future DataFrame and Forecast
+#Future Forecasting
 future = model.make_future_dataframe(periods=30)  # forecast 30 days into future
 forecast = model.predict(future)
-
-# STEP 4: Save Forecast Plots
 # Forecast Line Plot
 fig1 = model.plot(forecast)
 fig1.savefig("AAPL_forecast_plot.png")
-# Trend, Seasonality Components
+# trends and other componenets
 fig2 = model.plot_components(forecast)
 fig2.savefig("AAPL_forecast_components.png")
 
-# STEP 5: Save Forecast Data
 forecast.to_csv("AAPL_forecast_data.csv", index=False)
-print("Outputs saved.")
+print("Outputs saved.")# to verify the implementation of prophet model successfully
