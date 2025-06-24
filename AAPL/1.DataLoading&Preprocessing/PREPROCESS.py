1ed5133078ab167e0601d56e36755bf32ee69f1d
Shikhar Mishra <144837855+ShikharMishra16@users.noreply.github.com>
2025-06-24 18:26:19 +0530

    Update PREPROCESS.py

--- AAPL/1.DataLoading&Preprocessing/PREPROCESS.py
+++ AAPL/1.DataLoading&Preprocessing/PREPROCESS.py
@@ -4,57 +4,44 @@ from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from statsmodels.tsa.stattools import adfuller
 import matplotlib.pyplot as plt
 
-ticker = "AAPL"  
-start_date = "2015-01-01"
-end_date = "2024-12-31"
+ticker="AAPL"  
+startD="2015-01-01"
+endD="2024-12-31"
 
-raw_df = yf.download(ticker, start=start_date, end=end_date, auto_adjust=False)
-raw_df.to_csv("raw_data.csv")
-print("Raw data downloaded.")
-
+raww=yf.download(ticker,start=startD,end=endD,auto_adjust=False)
+raww.to_csv("rawD.csv")
 #CLEAN
-df = raw_df.copy()
-df.reset_index(inplace=True)
-df.dropna(inplace=True)
-df.drop_duplicates(inplace=True)
-df['Date'] = pd.to_datetime(df['Date'])
-df.sort_values('Date', inplace=True)
-df.set_index('Date', inplace=True)
-
-cleaned_df = df[['Close', 'High', 'Low']]  
-cleaned_df.to_csv("cleaned_data.csv")
-print("Data cleaned.")
-
+Data=raww.copy()
+Data.reset_index(inplace=True)
+Data.dropna(inplace=True)
+Data.drop_duplicates(inplace=True)
+Data['Date']=pd.to_datetime(Data['Date'])
+Data.sort_values('Date',inplace=True)
+Data.set_index('Date',inplace=True)
+CleanedD=Data[['Close','High','Low']]  
+CleanedD.to_csv("CleanedD.csv")
 #NORMALISING
-minmax_scaler = MinMaxScaler(feature_range=(0,1))
-normalized = pd.DataFrame(minmax_scaler.fit_transform(cleaned_df),
-                          columns=['Close_Norm', 'High_Norm', 'Low_Norm'],
-                          index=cleaned_df.index)
-normalized.to_csv("normalized_data.csv")
-print("Normalized data.")
-
+minmaxScaler=MinMaxScaler(feature_range=(0,1))
+normalized=pd.DataFrame(minmaxScaler.fit_transform(CleanedD),
+                          columns=['Close_Norm','High_Norm','Low_Norm'],
+                          index=CleanedD.index)
+normalized.to_csv("normalizedD.csv")
 #STANDARDIZING
-standard_scaler = StandardScaler()
-standardized = pd.DataFrame(standard_scaler.fit_transform(cleaned_df),
-                            columns=['Close_Std', 'High_Std', 'Low_Std'],
-                            index=cleaned_df.index)
-standardized.to_csv("standardized_data.csv")
-print("Standardized data.")
-
+standardScaler=StandardScaler()
+standardized=pd.DataFrame(standardScaler.fit_transform(CleanedD),
+                            columns=['Close_Std','High_Std','Low_Std'],
+                            index=CleanedD.index)
+standardized.to_csv("standardizedD.csv")
 #DETECTING STATIONARITY
-print("\nStationarity Test Results (ADF Test):")
-for col in cleaned_df.columns:
-    result = adfuller(cleaned_df[col])
-    print(f"\n🔹 {col}")
-    print(f"ADF Statistic: {result[0]:.4f}")
-    print(f"p-value: {result[1]:.4f}")
-    if result[1] < 0.05:
-        print("Likely Stationary")
+print("\nSTATIONARY TEST RESULTS:")
+for col in CleanedD.columns:
+    result=adfuller(CleanedD[col])
+    print(f"\n {col}")
+    print(f"Adfuller Statistic:{result[0]:.4f}")
+    print(f"p-value:{result[1]:.4f}")
+    if result[1]<0.05:
+        print("This Stock is Stationary!")
     else:
-        print("Likely Non-Stationary")
-plt.legend()
-plt.grid(True)
-plt.tight_layout()
-plt.show()
-# Step 8: Show the first few rows
+        print("This Stock is Non-Stationary!")
+#first few rows
 print(normalized.head())
